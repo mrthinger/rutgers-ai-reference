e196f1b5d58a7e9697a3ec4d393051afb496f459
Evan Pierce <evan@mrthinger.com>
2024-04-28 22:13:02 -0700

    dataset loading

--- data.py
+++ data.py
@@ -1,12 +1,81 @@
-train_img_path = "digitdata/trainingimages"
-train_label_path = "digitdata/traininglabels"
+from typing import Literal
+import numpy as np
 
-BLANK_LINE = (" " * 28) + "\n"
+DATASET_INFO = {
+    "digit": {
+        "images": {
+            "train": "digitdata/trainingimages",
+            "validation": "digitdata/validationimages",
+            "test": "digitdata/testimages",
+        },
+        "labels": {
+            "train": "digitdata/traininglabels",
+            "validation": "digitdata/validationlabels",
+            "test": "digitdata/testlabels",
+        },
+        "width": 28,
+        "height": 28,
+    },
+    "face": {
+        "images": {
+            "train": "facedata/facedatatrain",
+            "validation": "facedata/facedatavalidation",
+            "test": "facedata/facedatatest",
+        },
+        "labels": {
+            "train": "facedata/facedatatrainlabels",
+            "validation": "facedata/facedatavalidationlabels",
+            "test": "facedata/facedatatestlabels",
+        },
+        "width": 60,
+        "height": 70,
+    },
+}
 
-with open(train_img_path, "rt") as f:
-    train_x = f.readlines()
+
+def load_data(
+    dataset: Literal["digit", "face"],
+    split: Literal["train", "validation", "test"],
+):
+    info = DATASET_INFO[dataset]
+
+    # imgs
+    ipath = info["images"][split]
+    with open(ipath) as f:
+        ilines = f.readlines()
+
+    ibuf = np.zeros(shape=(len(ilines), info["width"]), dtype=np.uint8)
+
+    for row, line in enumerate(ilines):
+        for col, ch in enumerate(line):
+            if ch == " ":
+                pixel = 0
+            elif ch == "+":
+                pixel = 1
+            elif ch == "#":
+                pixel = 2
+            else:
+                continue
+            ibuf[row, col] = pixel
+
+    ibuf = ibuf.reshape(len(ilines) // info["height"], info["height"], info["width"])
+
+    # labels
+    lpath = info["labels"][split]
+    with open(lpath) as f:
+        llines = f.readlines()
+
+    lbuf = np.zeros(shape=(len(llines)), dtype=np.uint8)
+    for i, label in enumerate(llines):
+        lbuf[i] = int(label)
+
+    return ibuf, lbuf
 
 
-for line in train_x:
-    if line == BLANK_LINE:
-        print("line is blank")
+if __name__ == "__main__":
+    imgs, labels = load_data("digit", "train")
+    for img, label in zip(imgs, labels):
+        print(img, label)
+
+    # get first 2 10%s example
+    # imgs[: (imgs.shape[0] // 10) * 2]
